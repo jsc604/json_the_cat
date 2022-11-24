b5cf99a3c4f5e927af956f144aae375642de4034
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-24 15:12:30 -0800

    refractored code and implement mocha and chai test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,20 @@
 const request = require('request');
-let breed = process.argv[2];
-let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
-request(URL, (error, response, body) => {
-  try {
+const fetchBreedDescription = function(breedName, callback) {
+  let URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(URL, (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
     const data = JSON.parse(body);
-    console.log(data[0].description);
-  } catch (e) {
-    console.log(error);
-  }
-});
+    if (data[0]) {
+      return callback(null, data[0].description);
+    } else {
+      return callback('Breed not found');
+    }
+   
+  });
+};
+
+module.exports = { fetchBreedDescription };
